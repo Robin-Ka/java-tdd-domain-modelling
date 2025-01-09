fa223a8af193f582424b8963873d97b867d6a09a
Robin Kaga <robin.kaga@no.experis.com>
2025-01-09 10:41:32 +0100

    added files and first test

---
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -0,0 +1,23 @@
+package com.booleanuk.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+public class BasketTest {
+
+    @Test
+    public void addProductNotInBasket(){
+        // Setup
+        Basket basket = new Basket();
+        String productName = "crisps";
+        int price = 30;
+
+        // Execute and verify
+        Assertions.assertTrue(basket.add(productName, price));
+    }
+
+    
+
+}
